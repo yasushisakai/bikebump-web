92d1bb4fe58faf472bc2ba0758f42ba07020623a
yasushisakai <mail@yasushisakai.com>
2016-10-03 05:04:31 +0000

    working remotely in https://bikebump.yasushisakai.com

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 
-config.api_root = "http://104.131.105.112:8080/api/";
+config.api_root = "https://bikebump.yasushisakai.com/api/";
 //config.api_root = "http://localhost:8080/api/";
 
 module.exports = config;
