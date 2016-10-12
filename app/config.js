73c216e0f7de42a083dcbec51eb1cf1e12e899da
Yasushi Sakai <ysshski@gmail.com>
2016-10-12 08:57:56 -0400

    making it work right after cloning

--- app/config.js
+++ app/config.js
@@ -1,14 +1,15 @@
-var Env = require('./utilities/Env');
-
 var Config;
 
 Config = {
     isRemote:function(){
-        return Env.isRemote;
+
+        return false;
     },
     api_root:function(){
+
         let api_root = '/api/';
-        if(Env.isRemote){
+
+        if(this.isRemote()){
             api_root = 'https://bikebump.yasushisakai.com'+api_root;
         }else{
             api_root = 'http://localhost:8080'+api_root;
