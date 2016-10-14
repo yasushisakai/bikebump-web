0db99dc58ff141c1be16c446780ceca4b300985e
Yasushi Sakai <ysshski@gmail.com>
2016-10-14 08:27:07 -0400

    add heatmap module

--- app/components/MapCompoment.js
+++ app/components/MapCompoment.js
@@ -15,6 +15,7 @@ export default function MapComponent(props) {
 
     let position = [props.lat,props.lng];
 
+    let url = 'https://api.mapbox.com/styles/v1/yasushisakai/ciu8srn4u002v2jrxc81ty7as/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieWFzdXNoaXNha2FpIiwiYSI6ImNpdThwajN1ZTAwNjUzM28weHRuMnJ4a2kifQ.ooHi0pGR-SdDraWzTRCoVA';
 
     let fences = props.fences.map(pos=>{
        return(<Circle center={[pos[0],pos[1]]} radius={10}/>);
@@ -27,7 +28,7 @@ export default function MapComponent(props) {
             <Map center={position} zoom={props.zoom} style={{height:"100vh"}}>
                 <TileLayer
                     attribution="&copy; Mapbox &copy; OpenStreetMap &copy; DigitalGlobe"
-                    url='https://api.mapbox.com/styles/v1/yasushisakai/ciu8srn4u002v2jrxc81ty7as/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieWFzdXNoaXNha2FpIiwiYSI6ImNpdThwajN1ZTAwNjUzM28weHRuMnJ4a2kifQ.ooHi0pGR-SdDraWzTRCoVA'
+                    url= 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
                 {fences}
             </Map>
