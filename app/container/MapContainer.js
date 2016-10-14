69d40f671f7ea2b04d9033f5ac3cc07f983f89ab
Yasushi Sakai <ysshski@gmail.com>
2016-10-13 21:52:43 -0400

    basic map view

--- app/container/MapContainer.js
+++ app/container/MapContainer.js
@@ -5,7 +5,10 @@
 
 import React, {Component} from 'react';
 
-import Map from '../components/Map'
+import MapComponent from '../components/MapCompoment'
+import QuestionContainer from './QuestionContainer';
+
+import GeoLocationHelper from '../utilities/GeoLocationHelper'
 
 //
 // MapContainer class
@@ -14,17 +17,49 @@ export default class MapContainer extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+
+        this.state = {
+            fences: [],
+            lat : 42,
+            lng: -71,
+        };
+
+        console.log(this.state.lat);
+        GeoLocationHelper.getGeoLocation()
+            .then(function(position){
+                this.setState({
+                    lat:position.latitude,
+                    lng:position.longitude
+                });
+            }.bind(this));
+
+        QuestionContainer.getFenceListFromAPI()
+            .then((data)=>{
+                let coordinates = data.map(obj=>{
+                   return [obj.coordinates.lat,obj.coordinates.lng]
+                });
+
+                this.setState({
+                    fences:coordinates
+                })
+            })
+
     }
 
-    // getInitialState(){}
+    componentDidMount(){
+    }
+
     // componentDidMount(){}
     // componentDidUpdate(){}
     // componentWillUnmount(){}
 
+
     render() {
+
+        console.log(this.state.lat);
+
         return (
-          <Map/>
+          <MapComponent lat={this.state.lat} lng={this.state.lng} fences={this.state.fences}/>
         );
     }
 }
