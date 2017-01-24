cf262c19ef3bafe68b4db1ba665dbb8e3a774da5
Yasushi Sakai <ysshski@gmail.com>
2017-01-24 12:15:36 -0500

    for ramya

--- app/containers/MapVis/MapVisContainer.js
+++ app/containers/MapVis/MapVisContainer.js
@@ -3,6 +3,7 @@ import { MapVis } from 'components'
 import { bindActionCreators } from 'redux'
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
+import { toJS } from 'immutable'
 
 import * as dingFeedActionCreators from 'modules/dingFeed'
 import { findClosestRoad } from 'helpers/api'
@@ -12,12 +13,14 @@ const MapVisContainer = React.createClass({
   propTypes:{
     isFetching:PropTypes.bool.isRequired,
     handleSetDingListener:PropTypes.func.isRequired,
+    dings: PropTypes.array.isRequired,
   },
   componentDidMount () {
     // add dings and dingFeed to the state
     this.props.handleSetDingListener() 
   },
   render () {
+    console.log(this.props.dings)
     return (
       <MapVis isFetching={this.props.isFetching}/>
       )
@@ -26,8 +29,12 @@ const MapVisContainer = React.createClass({
 
 
 function mapStateToProps({dings, dingFeed}){
+  const justDings = dingFeed.get('dingIds').toJS().map((dingId)=>{
+    return dings.get(dingId).toJS()
+  })
   return {
     isFetching:dings.get('isFetching') || dingFeed.get('isFetching'),
+    dings:justDings,
   }
 }
 
