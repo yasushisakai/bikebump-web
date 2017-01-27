56155b0ec262b0cff2785f70afa7a88828867db9
Yasushi Sakai <ysshski@gmail.com>
2017-01-27 11:42:41 -0500

    initial styling I want to use mojs

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,5 +1,12 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
+import { navigation, navLink, actionLink, link, icon } from './styles.css'
+import Account from 'react-icons/lib/md/account-circle'
+import SignOut from 'react-icons/lib/fa/sign-out'
+import SignIn from 'react-icons/lib/fa/sign-in'
+import MapIcon from 'react-icons/lib/fa/map-o'
+import Record from 'react-icons/lib/md/radio-button-checked'
+import Home from 'react-icons/lib/md/home'
 
 Navigation.propTypes=NavLinks.propTypes=ActionLinks.propTypes={
   isAuthed : PropTypes.bool.isRequired,
@@ -7,30 +14,30 @@ Navigation.propTypes=NavLinks.propTypes=ActionLinks.propTypes={
 
 function NavLinks ({isAuthed}) {
   return isAuthed===true
-  ? <div>
-    <Link to='/record'>{'Record'}</Link>
-    <Link to='/map'>{'Map'}</Link>
+  ? <div className={navLink}>
+    <Link className={link} to='/record'><Record className={icon}/></Link>
+    <Link className={link} to='/map'><MapIcon className={icon}/></Link>
   </div>
-  : <div>
-    <Link to='/'>{'Home'}</Link>
-    <Link to='/map'>{'Map'}</Link>
+  : <div className={navLink}>
+    <Link className={link} to='/'><Home className={icon}/></Link>
+    <Link className={link} to='/map'><MapIcon className={icon}/></Link>
   </div>
 }
 
 function ActionLinks ({isAuthed}) {
   return isAuthed === true
-  ? <div>
-    <Link to='/user/testuserId'>{'user'}</Link>
-    <Link to='/logout'>{'logout'}</Link>
+  ? <div className={actionLink}>
+    <Link className={link} to='/user/testuserId'><Account className={icon}/></Link>
+    <Link className={link} to='/logout'><SignOut className={icon}/></Link>
   </div>
-  : <div>
-    <Link to='/signin'>{'log in/sign in'}</Link>
+  : <div className={actionLink}>
+    <Link className={link} to='/signin'><SignIn className={icon}/></Link>
   </div>
 }
 
 export default function Navigation ({isAuthed}) {
   return (
-    <div>{'Navigation'}
+    <div className={navigation}>
     <NavLinks isAuthed={isAuthed} />
     <ActionLinks isAuthed={isAuthed} />
     </div>
--- app/components/Record/Record.js
+++ app/components/Record/Record.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react'
-import {button} from './styles.css'
+import { mainButton, smallButton,  detailText } from './styles.css'
 import { Map } from 'immutable'
+
+import { recordContainer } from './styles.css'
 
 Record.propTypes={
   isRecording : PropTypes.bool.isRequired,
@@ -20,29 +22,36 @@ export default function Record (props) {
     props.onReportButtonClick(1)
   }
 
+  const buttonStyle = {
+    backgroundColor : props.isRecording ? '#ff0000' : '#444444'
+  }
+
+  function buttonColor(color,float) {
+    return {
+      backgroundColor: color,
+      float : float,
+    }
+  }
+
   return (
-   <div>
-    <div className={button} onClick={props.onRecordButtonClick}>
-      {props.isRecording === true
-        ?'stop recording'
-        :'start recording'
-      }
+   <div className={recordContainer}>
+    <div className={mainButton} style={buttonStyle} onClick={props.onRecordButtonClick}>
     </div>
     {props.isRecording === true && props.isFetchingLatLng === false
     ? (<div>
-      <div className={button} onClick={onReportGood}>{'good'}</div>
-      <div className={button} onClick={onReportBad}>{'bad'}</div>
+      <div className={smallButton} style={buttonColor('#0055ff','right')}onClick={onReportGood}></div>
+      <div className={smallButton} style={buttonColor('#ff5500','left')}onClick={onReportBad}></div>
       </div>
       )
     : null
     }
-    <div>
+    <div className={detailText}>
       {
       props.isRecording === true && props.isFetchingLatLng === false
         ?(`location: lat=${props.location.get('lat')}, lat=${props.location.get('lng')}`)
         :null
       }
     </div>
-   </div>
+    </div>
   )
 }
